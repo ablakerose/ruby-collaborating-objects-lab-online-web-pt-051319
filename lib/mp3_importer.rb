5c39ad7febe5d54b475330c392eb555263abb351
Blake Rose <bdecktor@gmail.com>
2019-06-15 17:45:34 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -1,7 +1,14 @@
+require 'pry'
 class MP3Importer
   attr_accessor :path
   def initialize(path)
     @path = path
   end
+
+  def files 
+    #return all file names in given path as an array
+    Dir.entries(path)
+  end 
+  
 
 end
