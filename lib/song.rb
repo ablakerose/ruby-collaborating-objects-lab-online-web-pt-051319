3087c4b9585bd06ddcc3a24b63beb926004aaaf3
Blake Rose <bdecktor@gmail.com>
2019-06-15 18:09:30 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -9,7 +9,8 @@ class Song
   def self.new_by_filename(filename)
     #"Michael Jackson - Black or White - pop.mp3"
     artist, name = filename.split(" - ")
-    name = self.new(name)
+    song = self.new(name)
+    song
   end
 
 
