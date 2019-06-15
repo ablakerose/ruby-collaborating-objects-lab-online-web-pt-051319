bc462c222b0a36fcef62d8854d1cf7f4504afeb0
Blake Rose <bdecktor@gmail.com>
2019-06-15 16:39:12 +0000

    Automatically backed up by Learn

---
+++ lib/artist.rb
@@ -0,0 +1,23 @@
+class Artist
+  attr_accessor :name
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+  end
+
+  def self.all
+    @@all
+  end
+
+  def save
+    self.class.all << self
+  end
+
+  def add_song(song)
+    song.save
+  end
+
+
+end
